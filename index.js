633af660ecdface07f309a47300a5b826e5032d8
tyler mcclain <email.tyler.mcclain@gmail.com>
2019-07-08 22:05:48 -0400

    adding routes, and installing joi

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 'use strict';
 
 const Hapi = require('@hapi/hapi');
+const Routes = require('./lib/routes');
 
 const init = async () => {
 
     const server = Hapi.server({
         port: 3000,
-        host: 'localhost'
+        host: '127.0.0.1'
     });
+
+    server.route(Routes);
 
     await server.start();
     console.log('Server running on %s', server.info.uri);
